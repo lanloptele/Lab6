f1b9f6ac66b304119146a2b5a80186bba1bb6e35
lanloptele <100698914+lanloptele@users.noreply.github.com>
2022-05-24 13:30:04 +0000

    nieskończony 6.2

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+
 (function () {
   //TODO//
  ex1_button.addEventListener("click", function() {
    ex1_content.innerHTML= "0,1,2,3,4,5,6,7,8,9";
  })
+   ex2_tekst.addEventListener("input", function() {
+     if(ex2_tekst.value.lenght ==9){
+       ex2_content.innerHTML= "Długość numeru musi być równa 9";}
+     else{
+       ex2_content.innerHTML= "Długość poprawna";}
+   })
 })();
